160140875fc6b331e47de6caca544d44f1e867f3
Bolu <mobolurin@vt.edu>
2024-11-21 15:17:25 -0500

    Added character class

---
+++ Character.java
@@ -0,0 +1,38 @@
+public class Character {
+    // Fields -------------------------------
+    
+    /* Charracter attributes */
+
+    private String name;
+    private double experience;
+    private int level;
+    private double health;
+    private double strength;
+    private double speed;
+    private double intelligence;
+
+
+
+
+
+
+    // Constructor -------------------------------
+    public Character(String name) {
+        
+    }
+
+
+    // Methods -------------------------------
+
+    
+    /**
+     * METHODS TO ADD
+     * -- equip weapon
+     * -- 
+     * 
+     * 
+     */
+
+
+
+}
---
+++ ProjectRunner.java
@@ -0,0 +1,7 @@
+public class ProjectRunner {
+    public static void main(String[[] args]) {
+        
+    }
+
+
+}
