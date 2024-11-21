6d3388dae35d2ee3da3a6002b4b103a673dd1954
Bolu <mobolurin@vt.edu>
2024-11-21 15:28:29 -0500

    Added Weapons + item interface

--- CharacterCreation.java
+++ CharacterCreation.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class ProjectRunner {
+public class CharacterCreation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
