0d313467d5fb275b8aac09fdf0776f4d73ca3908
Bolu <adekanmbimobolurin@gmail.com>
2024-12-06 23:55:14 -0500

    tiny upgrade changes

--- GameEntities/Upgrade.java
+++ GameEntities/Upgrade.java
@@ -5,8 +5,16 @@ public class Upgrade {
      *  UpgradeTypes range from upgrading food items, weapons, armour, etc.
      * This is to avoid  
      */
-    String upgradeType;
+    private String upgradeType;
     
+    public String getUpgradeType() {
+        return this.upgradeType;
+    }
+
+    public void setUpgradeType(String upgradeType) {
+        this.upgradeType = upgradeType;
+    }
+
 
 
 
