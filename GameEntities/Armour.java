abd47a7657c433ef147446c0b841e0e0219ada3f
Bolu <adekanmbimobolurin@gmail.com>
2024-12-12 22:21:26 -0500

    small getter/setter methods

--- GameEntities/Armour.java
+++ GameEntities/Armour.java
@@ -1,12 +1,51 @@
 package GameEntities;
 
 public class Armour {
-    int durability;
-    int health;
-    int speedBoost;
-    int strengthBoost;
+    private int durability;
+    private int health;
+    private int speedBoost;
+    private int strengthBoost;
     
     public Armour() {
 
     }
+
+    public int getDurability() {
+        return this.durability;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public int getSpeedBoost() {
+        return this.speedBoost;
+    }
+
+    public int getStrengthBoost() {
+        return this.strengthBoost;
+    }
+
+    public void setDurability(int newDurability) {
+        this.durability = newDurability;
+    }
+
+    public void setHealth(int newHealth) {
+        this.health = newHealth;
+    }
+
+    public void setSpeedBoost(int newSpeedBoost) {
+        this.speedBoost = newSpeedBoost;
+    }
+
+    public void setStrengthBoost(int newStrengthBoost) {
+        this.strengthBoost = newStrengthBoost;
+    }
+
+    public void appriase() {
+        // need to work on this functionality later
+    }
+
+
+
 }
