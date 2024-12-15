3b9e5f464411b73b19658f1fff866efcfab445cb
Bolu <adekanmbimobolurin@gmail.com>
2024-12-15 13:41:42 -0500

    chest class another form of storage

---
+++ GameEntities/Chest.java
@@ -0,0 +1,52 @@
+package GameEntities;
+
+public class Chest implements Inventory {
+    private int capacity;
+
+    /*
+     * Since chests will generally have a much larger capacity that inventory on
+     * hand I want to experiment with a linked list implementation since I switched
+     * from linked list to arrays for the bag class 
+     */
+
+
+     /*
+      * Additionally thinking of writing my own doubly linked list class to traverse
+      backwards and forwards in the ches and show some sort of scroall bar animation in regards
+      to which item we are currently on. It can be displayed at the bottom of the screen and
+      fetch the next couple of items to be visible (will need to work on this)
+      */
+
+
+
+    @Override
+    public int getCapacity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCapacity'");
+    }
+
+    @Override
+    public boolean addItem(Item newItem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addItem'");
+    }
+
+    @Override
+    public void shuffle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shuffle'");
+    }
+
+    @Override
+    public Item remove(Item anItem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean add(Item newItem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+    
+}
