3c154f6e88278efb383e1693e34adeddcc56f2c1
Bolu <adekanmbimobolurin@gmail.com>
2024-12-20 23:18:47 -0500

    working on the stack

--- DataStructures/Stack.java
+++ DataStructures/Stack.java
@@ -1,5 +1,17 @@
 package DataStructures;
 
-public class Stack {
+public class Stack<T> {
+    private Node<T> head;
+    private int size;
+    public static int CAPACITY;
+
+    public Stack() {
+        
+    }
+
+    public boolean add() {
+
+    }
     
+
 }
