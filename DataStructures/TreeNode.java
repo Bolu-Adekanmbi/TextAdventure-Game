3e8919b4407435aa7917ad1e6ab12233d15c7781
Bolu <adekanmbimobolurin@gmail.com>
2024-12-27 22:31:34 -0500

    signing off for today push

--- DataStructures/TreeNode.java
+++ DataStructures/TreeNode.java
@@ -1,9 +1,23 @@
 package DataStructures;
 
 public class TreeNode<T> extends Node<T> {
+    
+    private Node<T> left;
+    private Node<T> right; 
+
+
     public TreeNode(T data) {
         super(data);
+        this.left = null;
+        this.right = null;
     }
+
+    /*
+     * Need to add getter and setter methods for the left and right nodes
+     * Further, need to create a constructor for setting left, data, and right
+     * and subsequently change the default constructor
+     */
+
 
     @Override
     public Node<T> getNext() {
